b266e15b2905ce51fb9df8cdbf97a847ba4f6f58
Deven1902 <devenamazingnandapurkar@gmail.com>
2023-09-08 18:58:19 +0530

    rest api

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ const PORT = 3000;
 //mimic the db using an array
 let blogsList =[];
 
+app.get('/blogs', (req,res)=>{
+    return res.status(404).json({
+        data:blogsList,
+        success:true,
+
+    });
+});
+
+app.post('/blogs', (req,res)=>{
+    blogsList.push({title: req.body.title, content:req.body.content, id:Math.floor(Math.random()*1000)});
+    return res.status(201).json({success:true,});
+});
+
+app.get('blogs/:id',(req,res)=>{
+   const result =blogsList.filter((blog) => blog.id==req.params.id);
+return res.status(200).json({
+    data:result,
+    sucess:true
+})
+})
+
 
 app.listen(PORT, () => {
     console.log("Server started on PORT ", PORT);
